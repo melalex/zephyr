b8cf7c57f800d64e31a158544f3c2f15317298f4
melalex <melalex490@gmail.com>
2018-04-07 22:17:55 +0300

    Multimodule structure for keyword service

--- backend/keyword/services/src/main/java/com/zephyr/keyword/KeywordConfiguration.java
+++ backend/keyword/services/src/main/java/com/zephyr/keyword/KeywordConfiguration.java
@@ -8,20 +8,13 @@ import com.zephyr.keyword.properties.KeywordSource;
 import com.zephyr.keyword.services.provider.KeywordsProvider;
 import com.zephyr.keyword.services.provider.impl.AdWordsKeywordsProvider;
 import com.zephyr.keyword.services.provider.impl.RatingServiceProvider;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
 
-@EnableFeignClients
-@SpringBootApplication
-public class KeywordApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(KeywordApplication.class, args);
-    }
+@Configuration
+public class KeywordConfiguration {
 
     @Bean
     public Manager<KeywordSource, KeywordsProvider> keywordManager() {
