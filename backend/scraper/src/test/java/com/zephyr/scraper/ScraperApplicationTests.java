d379996f77c1d48e6765b87c717437137bfd59a7
melalex <melalex490@gmail.com>
2018-04-30 19:01:01 +0300

    Add ScraperApplicationTests

--- backend/scraper/src/test/java/com/zephyr/scraper/ScraperApplicationTests.java
+++ backend/scraper/src/test/java/com/zephyr/scraper/ScraperApplicationTests.java
@@ -1,16 +1,47 @@
 package com.zephyr.scraper;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.cloud.stream.test.matcher.MessageQueueMatcher.receivesPayloadThat;
+
+import com.zephyr.data.internal.dto.QueryDto;
+import com.zephyr.test.CommonTestData;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.concurrent.BlockingQueue;
+
 @SpringBootTest
+@ActiveProfiles("streamTest")
 @RunWith(SpringRunner.class)
+@Import(ScraperTestConfiguration.class)
 public class ScraperApplicationTests {
+
+    @Autowired
+    private Processor processor;
+
+    @Autowired
+    private MessageCollector collector;
 
     @Test
     public void contextLoads() {
+        Message<QueryDto> testMessage = new GenericMessage<>(CommonTestData.queries().simple());
 
+        processor.input().send(testMessage);
+
+        BlockingQueue<Message<?>> actual = collector.forChannel(processor.output());
+
+        assertThat(actual, receivesPayloadThat(is(CommonTestData.searchResults().bing())));
+        assertThat(actual, receivesPayloadThat(is(CommonTestData.searchResults().google())));
+        assertThat(actual, receivesPayloadThat(is(CommonTestData.searchResults().yahoo())));
     }
 }
