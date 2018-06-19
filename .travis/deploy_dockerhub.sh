142cef2ac998ec887ee70e621696a474038f7371
melalex <melalex490@gmail.com>
2018-06-19 10:24:51 +0300

    update docker script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" == "develop" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ]; then
     ./gradlew dockerPushImage -PdockerUserName="$DOCKER_LOGIN" -PdockerPassword="$DOCKER_PASSWORD" -PtagVersion="latest";
 fi
